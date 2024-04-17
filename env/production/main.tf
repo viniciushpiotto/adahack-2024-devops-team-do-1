fea5b01eb3dee1845b01ba50600757701fb9e198
piotto <viniciuspiotto.dev@gmail.com>
2024-04-17 01:14:39 -0300

    fix: terraform region

--- env/production/main.tf
+++ env/production/main.tf
@@ -6,7 +6,7 @@ module "Production" {
   region = "us-east-1"
   name = "production"
   description = "application-for-production"
-  enviroment_beanstalk_name = "adahack-2024-devops-team-do-1-prod"
+  enviroment_beanstalk_name = "enviroment-for-production"
   type_instance = "t2.micro"
   max_size = 5
 }
